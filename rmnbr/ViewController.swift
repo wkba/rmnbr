f6cb7ce6560bf314df29d48ef87bff41c2691ef8
wakabashi <wkba61@gmail.com>
2016-09-06 20:11:08 +0530

    Initial Commit

---
+++ rmnbr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rmnbr
+//
+//  Created by wakabashi on 2016/09/06.
+//  Copyright © 2016年 wakabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
